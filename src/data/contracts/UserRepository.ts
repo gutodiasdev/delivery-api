5bd6492bd04e17de388d95be4f24585f56a89780
Augusto Dias <gutodiasux@gmail.com>
2023-02-20 20:35:25 -0300

    refact: make name as a required parameter to register new user

--- src/data/contracts/UserRepository.ts
+++ src/data/contracts/UserRepository.ts
@@ -1,11 +1,16 @@
 export interface CreateUserRepository {
-  create: (input: CreateUserRepository.Input) => Promise<void>
+  create: (input: CreateUserRepository.Input) => Promise<CreateUserRepository.Output>
 }
 
 export namespace CreateUserRepository {
   export type Input = {
-    name?: string
+    name: string
     email: string
     password: string
+  }
+
+  export type Output = {
+    name: string
+    email: string
   }
 }
--- src/data/prisma/UserRepository.ts
+++ src/data/prisma/UserRepository.ts
@@ -1,5 +1,23 @@
-export class UserRepository {
-  async create(): Promise<void> {
+import { CreateUserRepository } from '@/data/contracts'
+import { prisma } from '@/main/config/prisma'
 
+export class UserRepository implements CreateUserRepository {
+  async create(input: CreateUserRepository.Input): Promise<CreateUserRepository.Output> {
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          password: input.password
+        }
+      })
+
+      return {
+        name: user.name,
+        email: user.email
+      }
+    } catch {
+      throw new Error('Failed to create new user')
+    }
   }
 }
