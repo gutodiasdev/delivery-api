ede8bbad811a594eabc601857a1bf92678b81408
Augusto Dias <gutodiasux@gmail.com>
2023-02-21 22:18:28 -0300

    feat: add UserAlreadyExists and FailedGenerateRefreshToken errors

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,31 +1,50 @@
 import { sign } from 'jsonwebtoken'
 import { v4 as uuid } from 'uuid'
 
-import { CreateUserRepository, UpdateRefreshTokenRepository } from '@/data/contracts'
+import {
+  CreateUserRepository,
+  FindByEmailRepository,
+  UpdateRefreshTokenRepository
+} from '@/data/contracts'
+import { AppError, HttpCode } from '@/errors'
 import { CreateUser } from '@/interfaces'
 
 export class CreateUserService implements CreateUser {
   constructor(
-    private readonly userRepository: CreateUserRepository & UpdateRefreshTokenRepository
+    private readonly userRepository: CreateUserRepository & UpdateRefreshTokenRepository & FindByEmailRepository
   ) { }
 
   async execute(input: CreateUser.Input): Promise<CreateUser.Output> {
     const { name, email, password } = input
-    try {
-      await this.userRepository.create(input)
 
-      const refreshToken = uuid()
+    const userAlreadyExists = await this.userRepository.findByemail({ email })
+    if (userAlreadyExists) {
+      throw new AppError({
+        name: 'UserAlreadyExists',
+        description: 'Failed to create new user. Already exists in our database',
+        httpCode: HttpCode.BAD_REQUEST
+      })
+    }
 
-      await this.userRepository.updateRefreshToken({ email, refreshToken })
+    await this.userRepository.create(input)
 
-      const token = sign({ name, email, password }, 'SECRET', { expiresIn: '15m' })
+    const refreshToken = uuid()
 
-      return {
-        token,
-        refreshToken
-      }
-    } catch {
-      throw new Error('Failed to create new user')
+    if (!refreshToken) {
+      throw new AppError({
+        name: 'FailedGenerateRefreshToken',
+        description: 'Failed to generate a new refresh token',
+        httpCode: HttpCode.INTERNAL_SERVER_ERROR
+      })
+    }
+
+    await this.userRepository.updateRefreshToken({ email, refreshToken })
+
+    const token = sign({ name, email, password }, 'SECRET', { expiresIn: '15m' })
+
+    return {
+      token,
+      refreshToken
     }
   }
 }
